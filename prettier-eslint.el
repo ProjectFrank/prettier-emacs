8791cf2468721993ec7f61e5714532dd6e5168a1
Frank Wang <frank.wangtao@gmail.com>
2017-09-01 09:28:41 -0400

    Use prettier-eslint instead of prettier

--- prettier-eslint.el
+++ prettier-eslint.el
@@ -43,48 +43,35 @@
 
 ;;; Code:
 
-(defgroup prettier-js nil
+(defgroup prettier-eslint nil
   "Minor mode to format JS code on file save"
   :group 'languages
-  :prefix "prettier-js"
-  :link '(url-link :tag "Repository" "https://github.com/prettier/prettier"))
+  :prefix "prettier-eslint"
+  :link '(url-link :tag "Repository" "https://github.com/ProjectFrank/prettier-eslint-emacs"))
 
-(defcustom prettier-js-command "prettier"
-  "The 'prettier' command."
-  :type 'string
-  :group 'prettier-js)
-
-(defcustom prettier-js-args '()
+(defcustom prettier-eslint-args '()
   "List of args to send to prettier command."
   :type '(repeat string)
-  :group 'prettier-js)
+  :group 'prettier-eslint)
 
-(defcustom prettier-js-show-errors 'buffer
-    "Where to display prettier error output.
+(defcustom prettier-eslint-show-errors 'buffer
+  "Where to display prettier error output.
 It can either be displayed in its own buffer, in the echo area, or not at all.
 Please note that Emacs outputs to the echo area when writing
 files and will overwrite prettier's echo output if used from inside
 a `before-save-hook'."
-    :type '(choice
-            (const :tag "Own buffer" buffer)
-            (const :tag "Echo area" echo)
-            (const :tag "None" nil))
-      :group 'prettier-js)
-
-(defcustom prettier-js-width-mode nil
-  "Specify width when formatting buffer contents."
   :type '(choice
-          (const :tag "Window width" window)
-          (const :tag "Fill column" fill)
+          (const :tag "Own buffer" buffer)
+          (const :tag "Echo area" echo)
           (const :tag "None" nil))
-  :group 'prettier-js)
+  :group 'prettier-eslint)
 
-(defun prettier-js--goto-line (line)
+(defun prettier-eslint--goto-line (line)
   "Move cursor to line LINE."
   (goto-char (point-min))
-    (forward-line (1- line)))
+  (forward-line (1- line)))
 
-(defun prettier-js--apply-rcs-patch (patch-buffer)
+(defun prettier-eslint--apply-rcs-patch (patch-buffer)
   "Apply an RCS-formatted diff from PATCH-BUFFER to the current buffer."
   (let ((target-buffer (current-buffer))
         ;; Relative offset between buffer line numbers and line numbers
@@ -103,7 +90,7 @@ a `before-save-hook'."
         (goto-char (point-min))
         (while (not (eobp))
           (unless (looking-at "^\\([ad]\\)\\([0-9]+\\) \\([0-9]+\\)")
-            (error "Invalid rcs patch or internal error in prettier-js--apply-rcs-patch"))
+            (error "Invalid rcs patch or internal error in prettier-eslint--apply-rcs-patch"))
           (forward-line)
           (let ((action (match-string 1))
                 (from (string-to-number (match-string 2)))
@@ -120,20 +107,20 @@ a `before-save-hook'."
                     (insert text)))))
              ((equal action "d")
               (with-current-buffer target-buffer
-                (prettier-js--goto-line (- from line-offset))
+                (prettier-eslint--goto-line (- from line-offset))
                 (setq line-offset (+ line-offset len))
                 (kill-whole-line len)
                 (setq kill-ring (cdr kill-ring))))
              (t
-              (error "Invalid rcs patch or internal error in prettier-js--apply-rcs-patch")))))))))
+              (error "Invalid rcs patch or internal error in prettier-eslint--apply-rcs-patch")))))))))
 
-(defun prettier-js--process-errors (filename tmpfile errorfile errbuf)
+(defun prettier-eslint--process-errors (filename tmpfile errorfile errbuf)
   "Process errors for FILENAME, using a TMPFILE an ERRORFILE and display the output in ERRBUF."
   (with-current-buffer errbuf
-    (if (eq prettier-js-show-errors 'echo)
+    (if (eq prettier-eslint-show-errors 'echo)
         (progn
           (message "%s" (buffer-string))
-          (prettier-js--kill-error-buffer errbuf))
+          (prettier-eslint--kill-error-buffer errbuf))
       (insert-file-contents errorfile nil nil nil)
       ;; Convert the prettier stderr to something understood by the compilation mode.
       (goto-char (point-min))
@@ -143,7 +130,7 @@ a `before-save-hook'."
       (compilation-mode)
       (display-buffer errbuf))))
 
-(defun prettier-js--kill-error-buffer (errbuf)
+(defun prettier-eslint--kill-error-buffer (errbuf)
   "Kill buffer ERRBUF."
   (let ((win (get-buffer-window errbuf)))
     (if win
@@ -152,61 +139,87 @@ a `before-save-hook'."
         (erase-buffer))
       (kill-buffer errbuf))))
 
-(defun prettier-js ()
-   "Format the current buffer according to the prettier tool."
-   (interactive)
-   (let* ((ext (file-name-extension buffer-file-name t))
-          (bufferfile (make-temp-file "prettier" nil ext))
-          (outputfile (make-temp-file "prettier" nil ext))
-          (errorfile (make-temp-file "prettier" nil ext))
-          (errbuf (if prettier-js-show-errors (get-buffer-create "*prettier errors*")))
-          (patchbuf (get-buffer-create "*prettier patch*"))
-          (coding-system-for-read 'utf-8)
-          (coding-system-for-write 'utf-8)
-          (width-args
-           (cond
-            ((equal prettier-js-width-mode 'window)
-             (list "--print-width" (number-to-string (window-body-width))))
-            ((equal prettier-js-width-mode 'fill)
-             (list "--print-width" (number-to-string fill-column)))
-            (t
-             '()))))
-     (unwind-protect
-         (save-restriction
-           (widen)
-           (write-region nil nil bufferfile)
-           (if errbuf
-               (with-current-buffer errbuf
-                 (setq buffer-read-only nil)
-                 (erase-buffer)))
-           (with-current-buffer patchbuf
-             (erase-buffer))
-           (if (zerop (apply 'call-process
-                             prettier-js-command bufferfile (list (list :file outputfile) errorfile)
-                             nil (append prettier-js-args width-args (list "--stdin" "--stdin-filepath" buffer-file-name))))
-               (progn
-                 (call-process-region (point-min) (point-max) "diff" nil patchbuf nil "-n" "--strip-trailing-cr" "-"
-                                      outputfile)
-                 (prettier-js--apply-rcs-patch patchbuf)
-                 (message "Applied prettier with args `%s'" prettier-js-args)
-                 (if errbuf (prettier-js--kill-error-buffer errbuf)))
-             (message "Could not apply prettier")
-             (if errbuf
-                 (prettier-js--process-errors (buffer-file-name) bufferfile errorfile errbuf))
-             ))
-       (kill-buffer patchbuf)
-       (delete-file errorfile)
-       (delete-file bufferfile)
-       (delete-file outputfile))))
+(defun prettier-eslint ()
+  "Format the current buffer according to the prettier tool."
+  (interactive)
+  (let* ((ext (file-name-extension buffer-file-name t))
+         (bufferfile (make-temp-file "prettier" nil ext))
+         (outputfile (make-temp-file "prettier" nil ext))
+         (errorfile (make-temp-file "prettier" nil ext))
+         (errbuf (if prettier-eslint-show-errors (get-buffer-create "*prettier errors*")))
+         (patchbuf (get-buffer-create "*prettier patch*"))
+         (eslintfile (expand-file-name
+                      ".eslintrc"
+                      (locate-dominating-file
+                       (or (buffer-file-name) default-directory)
+                       ".eslintrc")))
+         (eslint (expand-file-name
+                  "node_modules/eslint"
+                  (locate-dominating-file
+                   (or (buffer-file-name) default-directory)
+                   "node_modules")))
+         (prettier (expand-file-name
+                    "node_modules/prettier"
+                    (locate-dominating-file
+                     (or (buffer-file-name) default-directory)
+                     "node_modules")))
+         (prettier-eslint-command (expand-file-name
+                                   "node_modules/.bin/prettier-eslint"
+                                   (locate-dominating-file
+                                    (or (buffer-file-name) default-directory)
+                                    "node_modules")))
+         (coding-system-for-read 'utf-8)
+         (coding-system-for-write 'utf-8))
+    (unwind-protect
+        (save-restriction
+          (widen)
+          (write-region nil nil bufferfile)
+          (if errbuf
+              (with-current-buffer errbuf
+                (setq buffer-read-only nil)
+                (erase-buffer)))
+          (with-current-buffer patchbuf
+            (erase-buffer))
+          (if (zerop (apply 'call-process
+                            prettier-eslint-command
+                            bufferfile (list (list :file outputfile) errorfile)
+                            nil (append prettier-eslint-args
+                                        (list (buffer-file-name)
+                                              "--stdin"
+                                              "--eslint-path"
+                                              eslint
+                                              "--prettier-path"
+                                              prettier))))
+              (progn
+                (call-process-region (point-min)
+                                     (point-max)
+                                     "diff"
+                                     nil
+                                     patchbuf
+                                     nil
+                                     "-n"
+                                     "--strip-trailing-cr"
+                                     "-"
+                                     outputfile)
+                (prettier-eslint--apply-rcs-patch patchbuf)
+                (message "Applied prettier with args `%s'" prettier-eslint-args)
+                (if errbuf (prettier-eslint--kill-error-buffer errbuf)))
+            (message "Could not apply prettier")
+            (if errbuf
+                (prettier-eslint--process-errors (buffer-file-name) bufferfile errorfile errbuf))))
+      (kill-buffer patchbuf)
+      (delete-file errorfile)
+      (delete-file bufferfile)
+      (delete-file outputfile))))
 
 ;;;###autoload
-(define-minor-mode prettier-js-mode
+(define-minor-mode prettier-eslint-mode
   "Runs prettier on file save when this mode is turned on"
   :lighter " Prettier"
   :global nil
-  (if prettier-js-mode
-      (add-hook 'before-save-hook 'prettier-js nil 'local)
-    (remove-hook 'before-save-hook 'prettier-js 'local)))
+  (if prettier-eslint-mode
+      (add-hook 'before-save-hook 'prettier-eslint nil 'local)
+    (remove-hook 'before-save-hook 'prettier-eslint 'local)))
 
-(provide 'prettier-js)
-;;; prettier-js.el ends here
+(provide 'prettier-eslint)
+;;; prettier-eslint.el ends here
